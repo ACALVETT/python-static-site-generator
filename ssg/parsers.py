428bc0e36325cae309d9940cd7c0d107df534e25
ACALVETT <andrew@calvett.co.uk>
2021-09-14 18:13:37 +0100

    s

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import shutil
 import sys
 from docutils.core import publish_parts
-from ssg.content import content
+from ssg.content import Content
 from markdown import markdown
 
 class Parser:
@@ -36,7 +36,20 @@ class ResourceParser(Parser):
 class MarkdownParser:
     extensions = [".md",".markdown"]
 
-    def parse(self, path: Path, source: Path, dest: Path)):
-        Content.load()
-        content - self.read(path)
-    
+    def parse(self, path, source, dest)):
+        content = Content.load(self.read(path))
+        html = markdown(content.body)
+        self.write(path, dest, html)
+        sys.stdout.write(
+        "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
+        )
+
+class ReStructuredTextParser:
+
+    def parse(self, path, source, dest):
+        content = Content.load(self.read(path))
+        html = publish_parts(content.body,writer_name = "html5")
+        self.write(path,dest,html["html_body"])
+        sys.stdout.write(
+            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
+        )
